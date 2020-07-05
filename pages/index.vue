f0e9603fc574253b4bea41c02b10e929bcf57739
Ahoussou Jean-Chris Arnaud <shin.ahoussou@gmail.com>
2020-07-05 16:06:48 +0000

    vuetify initialisation commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-layout column align-center>
+      <v-avatar size="200">
+        <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="ahoussou" />
+      </v-avatar>
+      <span>DEVELOPPEUR WEB</span>
+    </v-layout>
+    <!-- divider -->
+    <v-container fluid>
+      <v-card class="mx-auto" max-width="1200" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div class="overline mb-4">OVERLINE</div>
+            <v-list-item-title class="headline mb-1"
+              >Headline 5</v-list-item-title
+            >
+            <v-list-item-subtitle
+              >Greyhound divisely hello coldly
+              fonwderfully</v-list-item-subtitle
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
